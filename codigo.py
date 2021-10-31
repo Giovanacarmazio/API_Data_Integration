b3a88fde55fe7a24609429d378c9da33a6c5bb99
Giovana Carmazio <83242981+Giovanacarmazio@users.noreply.github.com>
2021-10-31 19:51:40 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,5 +1,4 @@
 #1
-
 paciente_endereco_colibgeMunicipio
 paciente_endereco_coPais
 paciente_endereco_nmPais
@@ -8,5 +7,14 @@ paciente_endereco_uf
 estabelecimento_municipio_codigo
 estabelecimento_municipio_nome
 estabelecimento_uf
-
 #Sigilode dados dos entrevistados.
+
+
+#2
+vacina_dataAplicacao: Data imuni
+paciente_dataNascimento: Data de nascimento do paciente
+#menos granular é data imuni 
+
+#3
+@timestanp
+#porque o governo geralmente não publica dadoscom atualização em segundos.
